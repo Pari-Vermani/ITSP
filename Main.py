cd2240b9b7139da79912c7c55c15dbd72fac0b6e
arujakhanna <61024298+arujakhanna@users.noreply.github.com>
2020-06-22 12:24:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,20 @@
-#This is the main file for calling all the functions in order
-
+#This is the main file for calling all the functions in order.
+#Import Libraries
 import numpy as np
 import pandas as pd
 from matplotlib.image as mpimg
 import os
+
+#Import Functions
+from filename.innerfile import*
+
+def main():
+  #Import csv file with the data values
+  acc_inputs=pd.read_csv("ITSP/____.csv")
+  
+  print('csv file read')
+  
+  #Calling the functions
+  signal_mat= signal_processing_func(acc_inputs)
+  print('Signal processing funstion called successfully')
+  
