3072c5d713a08582a29b9ac44eaa2d38bea9936b
bvcxz1 <62253557+bvcxz1@users.noreply.github.com>
2020-06-26 10:21:53 +0530

    Add files via upload

--- Test Project/High Pass Filter/Noise_model.m
+++ Test Project/High Pass Filter/Noise_model.m
@@ -1,12 +1,16 @@
-n = 1000;
+n = 200;
 m = 5*n;
-x = linspace(0, 10*pi, m);
-x = reshape(x, [m, 1]);
-y = random('Normal', 9.8, 0.3) + sin(x);
+filename = 'M10.xlsx';
+t = linspace(0, 10*pi, m);
+t = reshape(t, [m, 1]);
+x = zeros(m, 1);
+x = random('Normal', x, 0.1);
+y = random('Normal', 9.8, 0.3) + sin(t);
 y = random('Normal', y, 0.1);
 z = zeros(m, 1);
-writematrix([x,y,z],'M7.xlsx','Sheet',1,'Range','A1:C5000')
-plot(x,y)
-xlabel('x')
-ylabel('sin(x)')
+z = random('Normal', z, 0.1);
+writematrix([x,y,z],filename,'Sheet',1,'Range','A1:C1000')
+plot(t,y)
+xlabel('t')
+ylabel('sin(t)')
 title('Plot of the Sine Function')
