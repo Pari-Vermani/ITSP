7d6935995df26b406130df865b5f69ba9eeade5c
bvcxz1 <62253557+bvcxz1@users.noreply.github.com>
2020-06-26 09:56:19 +0530

    Update Noise_model.m

--- Test Project/Moving Average Filter/Noise_model.m
+++ Test Project/Moving Average Filter/Noise_model.m
@@ -1,12 +1,16 @@
-n = 100;
+n = 1000;
+filename = 'M7.xlsx';
 m = 5*n;
-x = linspace(0, 10*pi, m);
-x = reshape(x, [m, 1]);
-y = sin(x);
+t = linspace(0, 10*pi, m);
+t = reshape(t, [m, 1]);
+x = zeros(m, 1);
+x = random('Normal', x, 0.1);
+y = sin(t);
 y = random('Normal', y, 0.1);
 z = zeros(m, 1);
-writematrix([x,y,z],'M1.xlsx','Sheet',1,'Range','A1:C500')
-plot(x,y)
-xlabel('x')
-ylabel('sin(x)')
+z = random('Normal', z, 0.1);
+writematrix([x,y,z],filename,'Sheet',1,'Range','A1:C5000')
+plot(t,y)
+xlabel('t')
+ylabel('sin(t)')
 title('Plot of the Sine Function')
